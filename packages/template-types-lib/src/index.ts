32d3d52b3fdcff3d4d1fd590378ae521d8d1919b
timonteutelink <timon@teutelink.nl>
2025-05-04 23:46:47 +0200

    very nice

--- packages/template-types-lib/src/index.ts
+++ packages/template-types-lib/src/index.ts
@@ -1,1 +1,22 @@
-export * from "./types/index";
+export type {
+	UserTemplateSettings,
+	StringOrCallback,
+	AnyOrCallback,
+	TemplateSettingsType,
+	TemplateConfig,
+	SideEffectFunction,
+	SideEffect,
+	RedirectFile,
+	TemplateDisablingThis,
+	AllowOverwrite,
+	AutoInstantiatedSubtemplate,
+	TemplateCommand,
+	LLMTools,
+	AiContext,
+	AiCallbackFunction,
+	AiAutoGenerateSettings,
+	AiUserConversationSettings,
+	TemplateConfigModule,
+} from "./types";
+
+export { templateConfigSchema } from "./types";
