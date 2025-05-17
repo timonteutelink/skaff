ea0f8ebb6f9f4dfd161aa1ba98a05a9977b873a4
timonteutelink <timon@teutelink.nl>
2025-05-17 20:29:00 +0200

    very nice

--- nix/flake/overlays/package.nix
+++ nix/flake/overlays/package.nix
@@ -1,7 +1,7 @@
 localFlake:
 { lib, config, self, inputs, ... }: {
   flake.overlays.default = final: prev: {
-    code-templator-cli = config.flake.packages.${prev.stdenv.hostPlatform.system}.code-templator-cli;
+    timon-code-templator-cli = config.flake.packages.${prev.stdenv.hostPlatform.system}.code-templator-cli;
   };
 }
 
