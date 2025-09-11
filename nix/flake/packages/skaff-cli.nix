918a4f0d16caab1faa2f948cb4c4bdd5fd9eb642
timonteutelink <timon@teutelink.nl>
2025-09-11 23:22:06 +0200

    very nice

--- nix/flake/packages/skaff-cli.nix
+++ nix/flake/packages/skaff-cli.nix
@@ -2,8 +2,11 @@ localFlake:
 { lib, config, self, inputs, ... }: {
   perSystem = { pkgs, system, ... }:
     {
-      packages.skaff-cli = pkgs.callPackage ./../../skaff-package/cli-package.nix {
-        inherit (inputs.bun2nix.lib.${system}) mkBunDerivation;
+      packages = {
+        skaff-cli = pkgs.callPackage ./../../skaff-package/cli-package.nix {
+          inherit (inputs.bun2nix.lib.${system}) mkBunDerivation;
+        };
+        default = self.packages.${system}.skaff-cli;
       };
     };
 
