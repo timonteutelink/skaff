d598f7b349eb5fcfc2954151c46646dcc0faec11
timonteutelink <timon@teutelink.nl>
2025-09-12 03:26:28 +0200

    very nice

--- nix/skaff-package/web-package.nix
+++ nix/skaff-package/web-package.nix
@@ -36,7 +36,14 @@ mkBunDerivation {
 
     cd apps/web
 
-    cp -r .next/standalone/* $out/share/app/
+    cp -r .next/standalone/apps/web/* $out/share/app/
+    cp -r .next/standalone/apps/web/.next $out/share/app/.next
+
+    cp -r .next/standalone/node_modules $out/share/app/node_modules
+    if [ -f .next/standalone/package.json ]; then
+      cp .next/standalone/package.json $out/share/app/package.json
+    fi
+
     cp -r public $out/share/app/public
     mkdir -p $out/share/app/.next
     cp -r .next/static $out/share/app/.next/static
@@ -48,6 +55,7 @@ mkBunDerivation {
       --set-default HOSTNAME 0.0.0.0 \
       --set-default PORT 3000 \
       --set NODE_ENV production \
+      --set NODE_PATH "$out/share/app/node_modules" \
       --set-default NEXT_CACHE_DIR /var/cache/skaff-web
 
     runHook postInstall
